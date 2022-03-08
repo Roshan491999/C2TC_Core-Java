d0988d088b9ef17c916891f7fdd163a83e52b24a
roshan <roshan@LAPTOP-4PM07022>
2022-03-08 22:16:27 +0530

    first commit

---
+++ Day2/src/Day2/Class.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class Class {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("area of a circle");
+		float pi=(float)3.14,radius=5 ;
+		float area = pi*radius*radius;
+				System.out.println(area);
+	}
+ 
+
+}
