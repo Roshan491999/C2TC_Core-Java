3f869874ffcaa7ba5a44b2836061b3e38514a605
roshan <roshan@LAPTOP-4PM07022>
2022-03-26 11:08:50 +0530

    first commit

--- Applet/src/com/applet/AppletDemo.java
+++ Applet/src/com/applet/AppletDemo.java
@@ -1,7 +1,14 @@
 package com.applet;
 
 import java.applet.Applet;
+import java.awt.*;
+
 
 public class AppletDemo extends Applet{
-
-}
+ Button b;
+ TextField t;
+   public void init() {b = new Button("submit");
+   t = new TextField();
+   add(b);
+   add(t);
+}}
