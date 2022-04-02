ad5ab38f567d4be764741591b4d1dc489b6548e1
roshan <roshan@LAPTOP-4PM07022>
2022-04-02 18:48:41 +0530

    first commit

---
+++ Saturday2/src/com/saturday/ReverseNumber.java
@@ -0,0 +1,23 @@
+package com.saturday;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number, reverse = 0; 
+		Scanner nu = new Scanner(System.in);
+		number = nu.nextInt();
+		while(number != 0)   
+		{  
+		int remainder = number % 10;  
+		reverse = reverse * 10 + remainder;  
+		number = number/10;  
+		}  
+		System.out.println("The reverse of the given number is: " + reverse);  
+		}  
+
+	}
+
+
