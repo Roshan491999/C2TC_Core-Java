4a5932f2cee2e9b01a4536074f0b8566167717e7
roshan <roshan@LAPTOP-4PM07022>
2022-04-02 18:42:26 +0530

    first commit

---
+++ Saturday2/src/com/saturday/FibonacciSeries.java
@@ -0,0 +1,28 @@
+package com.saturday;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int n1=0,n2=1,n3,i,count;
+          
+          Scanner s = new Scanner(System.in);
+           count = s.nextInt();
+           
+           System.out.print(n1 + " " + n2);
+           
+           for(i=2; i<=count; i++) 
+           {
+        	  
+        	   n3 = n1+n2;
+        	   System.out.print(" " +n3);
+        	   n1=n2;
+        	   n2=n3;
+           }
+
+
+	}
+
+}
