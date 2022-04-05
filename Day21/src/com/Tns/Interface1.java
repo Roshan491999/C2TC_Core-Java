a03f179a9c848ec811277ec48ae70865145d6484
roshan <roshan@LAPTOP-4PM07022>
2022-04-05 22:22:04 +0530

    first commit

---
+++ Day21/src/com/Tns/Interface1.java
@@ -0,0 +1,42 @@
+package com.Tns;
+
+final class A {
+	
+	public void meth(){
+		
+	}
+	
+}
+ 
+class B extends  A //here B cannot extends A because the class A declared as a final thus it gave error
+{
+	
+}
+
+class C{
+	int i=1,j;
+	 
+	final void add() {	
+		 j=i*2;
+		 System.out.println(j);
+		
+	}
+}
+
+class D extends C {           //- overrides com.Tns.C.add
+	                         // - Cannot override the final method from C
+	
+	
+	void add(){
+		
+	}
+}
+
+public class Interface1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
