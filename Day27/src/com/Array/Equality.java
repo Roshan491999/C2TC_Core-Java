7669150c2fc626815730f81e79ed33a5980843bc
roshan <roshan@LAPTOP-4PM07022>
2022-04-14 23:11:57 +0530

    first commit

---
+++ Day27/src/com/Array/Equality.java
@@ -0,0 +1,21 @@
+package com.Array;
+
+import java.util.Arrays;
+
+public class Equality {
+
+	public static void main(String[] args) {
+		int x[] = {1,4,8,3,4};
+		int y[] = {1,4,8,3,4};
+		
+		if(Arrays.equals(x,y)) {
+			System.out.println("Arrays Are equal");
+		}
+		else {
+			System.out.println("Arrays Are not equal");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
