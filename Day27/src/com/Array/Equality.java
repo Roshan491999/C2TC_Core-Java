91034771233f3edd56901a5ba3c065a6d0bf4786
roshan <roshan@LAPTOP-4PM07022>
2022-04-15 10:00:53 +0530

    first commit

--- Day27/src/com/Array/Equality.java
+++ Day27/src/com/Array/Equality.java
@@ -1,13 +1,25 @@
 package com.Array;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Equality {
 
 	public static void main(String[] args) {
-		int x[] = {1,4,8,3,4};
-		int y[] = {1,4,8,3,4};
-		
+		int x[] = new int[5];
+		int y[] = new int [5];
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter five elemnts of first array");
+		for(int i=0; i < x.length; i++) {
+		      // read input
+		      x[i] = scan.nextInt();
+		    }
+		System.out.println("enter five elemnts of second array");
+		for(int i=0; i < y.length; i++) {
+		      // read input
+		      y[i] = scan.nextInt();
+		    }
+
 		if(Arrays.equals(x,y)) {
 			System.out.println("Arrays Are equal");
 		}
