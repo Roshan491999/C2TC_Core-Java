2940929bbcbccefa899a8ca5e8906e48d0c09942
roshan <roshan@LAPTOP-4PM07022>
2022-04-25 22:58:13 +0530

    first commit

---
+++ Exception/src/com/Exception/Finally.java
@@ -0,0 +1,33 @@
+package com.Exception;
+
+public class Finally {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//parent try block
+				try {
+					
+					//child try block
+					try {
+						System.out.println("inside block 1");
+						int b=45/0;
+						System.out.println(b);
+						
+					}
+					catch(ArithmeticException e1) {
+						System.out.println("Exception :e1");
+					}
+				}
+				catch(ArithmeticException e3) {
+					System.out.println("Arithmetic Exception ");
+					System.out.println("inside parent try catch block");
+				}
+				finally {
+					System.out.println("its Executed");
+				
+				}
+
+
+	}
+
+}
