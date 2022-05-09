fc4ce7e6ae79ebcab0f4965630a7d3f29ae0830b
roshan <roshan@LAPTOP-4PM07022>
2022-05-09 20:37:05 +0530

    first commit

--- Practice/src/com/Practice/Test.java
+++ Practice/src/com/Practice/Test.java
@@ -7,15 +7,22 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=0 ,remainder=0;
+		
 		Scanner sc = new Scanner(System.in);
 		a =sc.nextInt();
+		
+		
 		if (a>10) 
 			System.out.println("given value is "+a);
 		else
 			System.out.println("enter value greater than 10");
 		
-		
-		if(a>100 & a<1000) {
+		if(a>10 & a<100) {
+			remainder=a%10;
+			a= a-remainder;
+			System.out.println(a);
+			}
+		else if(a>100 & a<1000) {
 		remainder=a%100;
 		a= a-remainder;
 		System.out.println(a);
@@ -45,13 +52,11 @@ public class Test {
 			a= a-remainder;
 			System.out.println(a);
 		}
-
-
-		
-		
-		
-		
-		
+		else if(a>100000000 & a<1000000000){
+			remainder=a%100000000;
+			a= a-remainder;
+			System.out.println(a);
+		}
 
 
 	}
